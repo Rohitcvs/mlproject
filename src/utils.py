5f3a0f0cb9ec8cb6333ebf4e86b6474ac804c0ec
Rohit Chintakindi <venkatasairohit@gmail.com>
2025-01-16 17:56:20 -0700

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,6 @@ import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -17,7 +16,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -39,7 +38,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
-            
+
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
@@ -49,7 +48,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
-    
+
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
